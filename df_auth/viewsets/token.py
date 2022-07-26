3d2e94257c0b683f9cf062c80d0821870b4b9144
Alexis Yushin <alexis@apexive.com>
2022-07-26 22:54:44 +0200

    initial working serializer

--- df_auth/viewsets/token.py
+++ df_auth/viewsets/token.py
@@ -1,20 +1,18 @@
-from rest_framework import mixins, viewsets
+from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.settings import import_string
 from rest_framework_simplejwt.settings import api_settings as sjwts
+from rest_framework_simplejwt.views import TokenViewBase
 
 
-class TokenViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
-    def perform_create(self, serializer):
-        pass
-
+class TokenViewSet(TokenViewBase, viewsets.GenericViewSet):
     @action(
         methods=["post"],
         detail=False,
         serializer_class=import_string(sjwts.TOKEN_REFRESH_SERIALIZER),
     )
     def refresh(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+        return self.post(request, *args, **kwargs)
 
     @action(
         methods=["post"],
@@ -22,7 +20,7 @@ class TokenViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
         serializer_class=import_string(sjwts.TOKEN_VERIFY_SERIALIZER),
     )
     def verify(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+        return self.post(request, *args, **kwargs)
 
     # @action(
     #     methods=["post"],
@@ -30,4 +28,4 @@ class TokenViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     #     serializer_class=import_string(sjwts.TOKEN_BLACKLIST_SERIALIZER),
     # )
     # def blacklist(self, request, *args, **kwargs):
-    #     return self.create(request, *args, **kwargs)
+    #     return self.post(request, *args, **kwargs)
