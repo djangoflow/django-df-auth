4ebab5571dad8f9228ebbb5c90f890a083c40cee
Alexis Yushin <alexis@apexive.com>
2022-07-27 09:04:37 +0200

    functional basic authentication

--- df_auth/serializers.py
+++ df_auth/serializers.py
@@ -1,19 +1,56 @@
+from .settings import api_settings
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import update_last_login
+from rest_framework import exceptions
 from rest_framework import serializers
-from rest_framework.settings import import_string
-
-import rest_framework_simplejwt.settings
+from rest_framework_simplejwt.settings import api_settings as simplejwt_settings
 
 
-# from rest_framework_simplejwt.views import *
+class TokenObtainSerializer(serializers.Serializer):
+    token = serializers.CharField(read_only=True)
+    token_class = simplejwt_settings.AUTH_TOKEN_CLASSES[0]
 
+    def validate(self, attrs):
+        attrs = super().validate(attrs)
+        self.user = authenticate(**attrs, **self.context)
 
-class OTPField(serializers.CharField):
-    pass
+        if not simplejwt_settings.USER_AUTHENTICATION_RULE(self.user):
+            raise exceptions.AuthenticationFailed()
 
+        token = self.get_token(self.user)
 
-class TokenObtainSerializer(
-    import_string(
-        rest_framework_simplejwt.settings.api_settings.TOKEN_OBTAIN_SERIALIZER
-    )
-):
-    otp = OTPField(write_only=True, required=False)
+        attrs["token"] = str(token)
+
+        if simplejwt_settings.UPDATE_LAST_LOGIN:
+            update_last_login(None, self.user)
+
+        return attrs
+
+    @classmethod
+    def get_token(cls, user):
+        return cls.token_class.for_user(user)
+
+    def get_fields(self):
+        fields = super().get_fields()
+        fields.update(
+            {
+                f: serializers.CharField(write_only=True, required=False)
+                for f in api_settings.USER_IDENTITY_FIELDS
+            }
+        )
+        if api_settings.REQUIRED_AUTH_FIELDS:
+            fields.update(
+                {
+                    f: serializers.CharField(write_only=True, required=True)
+                    for f in api_settings.REQUIRED_AUTH_FIELDS
+                }
+            )
+
+        if api_settings.OPTIONAL_AUTH_FIELDS:
+            fields.update(
+                {
+                    f: serializers.CharField(write_only=True, required=False)
+                    for f in api_settings.OPTIONAL_AUTH_FIELDS
+                }
+            )
+        return fields
--- df_auth/settings.py
+++ df_auth/settings.py
@@ -4,8 +4,8 @@ from rest_framework.settings import APISettings
 
 DEFAULTS = {
     "USER_IDENTITY_FIELDS": ("email",),
-    "PASSWORD_REQUIRED": False,
-    "OTP_REQUIRED": False,
+    "REQUIRED_AUTH_FIELDS": None,
+    "OPTIONAL_AUTH_FIELDS": ("otp", "password"),
 }
 
 api_settings = APISettings(getattr(settings, "DF_AUTH", {}), DEFAULTS)
