1d790ec77a232c6314997ea33133299d0dc9203e
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2022-08-01 19:44:10 +0430

    feat: add social connect view
    - a subclass of base auth view
    - return user data

--- df_auth/views/social_views.py
+++ df_auth/views/social_views.py
@@ -1,4 +1,7 @@
-from df_auth.serializers import OAuth2UserJWTSerializer
+from rest_framework.permissions import IsAuthenticated
+
+
+from df_auth.serializers import OAuth2UserJWTSerializer, OAuth2UserSerializer
 from df_auth.views.base import BaseSocialView
 
 
@@ -7,3 +10,10 @@ class SocialLoginView(BaseSocialView):
     class used for social authentications
     """
     response_serializer = OAuth2UserJWTSerializer
+
+class SocialConnectView(BaseSocialView):
+    """
+    class used for social accounts connection
+    """
+    permission_classes = (IsAuthenticated,)
+    response_serializer = OAuth2UserSerializer
