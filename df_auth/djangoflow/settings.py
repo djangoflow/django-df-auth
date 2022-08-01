e784d80f61128587e71932568af37c0519348528
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2022-08-01 19:40:12 +0430

    feat: add new settings
    - add allauth to APPS
    - add secret keys to read from env
    - add authentication backends

--- df_auth/djangoflow/settings.py
+++ df_auth/djangoflow/settings.py
@@ -1,4 +1,10 @@
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 DF_AUTH_APPS = [
+    'rest_framework',
     "rest_framework_simplejwt",
     "django_otp",
     "django_otp.plugins.otp_email",
@@ -6,4 +12,30 @@ DF_AUTH_APPS = [
     "django_otp.plugins.otp_static",
     "otp_twilio",
     "social_django",
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.apple',
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.github',
 ]
+
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.google.GoogleOAuth2',
+    'social_core.backends.github.GithubOAuth2',
+    'social_core.backends.apple.AppleIdAuth',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+SOCIAL_AUTH_RAISE_EXCEPTIONS = False
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = env('GOOGLE_OAUTH2_KEY')
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = env('GOOGLE_OAUTH2_SECRET')
+SOCIAL_AUTH_GITHUB_KEY = env('GITHUB_OAUTH2_KEY')
+SOCIAL_AUTH_GITHUB_SECRET = env('GITHUB_OAUTH2_SECRET')
+SOCIAL_AUTH_FACEBOOK_KEY = env('FACEBOOK_OAUTH2_KEY')
+SOCIAL_AUTH_FACEBOOK_SECRET = env('FACEBOOK_OAUTH2_SECRET')
+SOCIAL_AUTH_APPLE_ID_KEY = env('APPLE_ID_KEY')
+SOCIAL_AUTH_APPLE_ID_SECRET = env('APPLE_ID_SECRET')
