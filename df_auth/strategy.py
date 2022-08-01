f354882aba0f5943d471d82c22bffc70167e3ccf
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2022-08-01 19:46:56 +0430

    feat: add strategy
    - create drf strategy for social auth

---
+++ df_auth/strategy.py
@@ -0,0 +1,23 @@
+from social_django.strategy import DjangoStrategy
+
+
+class DRFStrategy(DjangoStrategy):
+
+    def __init__(self, storage, request=None, tpl=None):
+        self.request = request
+        self.session = {}
+
+        if request:
+            try:
+                self.session = request.session
+            except AttributeError:
+                # in case of token auth session can be disabled at all
+                pass
+
+        super(DjangoStrategy, self).__init__(storage, tpl)
+
+    def request_data(self, merge=True):
+        if self.request:
+            return getattr(self.request, 'auth_data', {})
+        else:
+            return {}
