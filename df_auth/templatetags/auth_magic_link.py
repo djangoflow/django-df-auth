7e6fe3f48880ff2aaf1df1ad8bd601468b09a79f
Alexis Yushin <alexis@apexive.com>
2022-08-09 09:37:05 +0200

    magic_link template tag

---
+++ df_auth/templatetags/auth_magic_link.py
@@ -0,0 +1,14 @@
+import base64
+
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def auth_magic_link(context):
+    """
+    :return: context("base_url") + base64.urlsafe_b64encode(context["username")/context("token"))
+    """
+    token = base64.urlsafe_b64encode(bytes(f"{context['username']}/{context['token']}", "utf-8")).decode("utf-8")
+    return f"{context['base_url']}{token}"
