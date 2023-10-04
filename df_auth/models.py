acd35c73558e6e5e08f27cc783880044c6100e07
Alexis Yushin <alexis@apexive.com>
2023-10-04 08:02:08 -0400

    wip

--- df_auth/models.py
+++ df_auth/models.py
@@ -2,14 +2,25 @@ from django.conf import settings
 from django.db import models
 
 
-class User2FA(models.Model):
+class UserOneToOneMixin:
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name="user_2fa",
     )
+
+
+class User2FA(UserOneToOneMixin, models.Model):
     is_required = models.BooleanField(default=False)
 
     class Meta:
         verbose_name = "User 2FA"
         verbose_name_plural = "User 2FA"
+
+
+class UserRegistration(UserOneToOneMixin, models.Model):
+    is_registering = models.BooleanField(default=True)
+    invited_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        related_name="user_2fa", null=True, blank=True
+    )
